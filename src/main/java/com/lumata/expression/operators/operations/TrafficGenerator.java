2f8b50fb5ce8106281b01dc44e112576772b39ed
Arcangelo Di Pasquale <arcangelo.dipasquale@lumatagroup.com>
2013-07-29 09:54:23 +0200

    Improving Traffic Generator

--- src/main/java/com/lumata/expression/operators/operations/TrafficGenerator.java
+++ src/main/java/com/lumata/expression/operators/operations/TrafficGenerator.java
@@ -1,5 +1,7 @@
 package com.lumata.expression.operators.operations;
 
+import org.json.JSONException;
+import org.json.JSONObject;
 import org.openqa.selenium.WebElement;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -7,7 +9,6 @@ import org.slf4j.LoggerFactory;
 import com.lumata.common.testing.log.Log;
 import com.lumata.common.testing.selenium.SeleniumUtils;
 import com.lumata.common.testing.selenium.SeleniumWebDriver;
-import com.lumata.expression.operators.campaigns.CampaignModel;
 
 public class TrafficGenerator extends Operations {
 
@@ -16,16 +17,46 @@ public class TrafficGenerator extends Operations {
 	public static boolean open( SeleniumWebDriver selenium, long timeout, long interval ) {
 				
 		if( !TrafficGenerator.select(selenium, timeout, interval) ) { return false; }
+				
+		logger.info( Log.CHECKING.createMessage( selenium.getTestName(), "for id=gwt-debug-InputOPTrafficGenerator") );
 		
-		/*
-		logger.info( Log.CHECKING.createMessage( selenium.getTestName(), "for id=gwt-debug-InputCMCampaignModel") );
+		WebElement trafficGeneratorButton = SeleniumUtils.findForComponentDisplayed(selenium, SeleniumUtils.SearchBy.ID, "gwt-debug-InputOPTrafficGenerator", timeout, interval);
+		if( trafficGeneratorButton == null ) { logger.error(  Log.FAILED.createMessage( selenium.getTestName() , "Cannot open the Traffic Generator DashBoard" ) ); return false; }	
 		
-		WebElement campaignModelButton = SeleniumUtils.findForComponentDisplayed(selenium, SeleniumUtils.SearchBy.ID, "gwt-debug-InputCMCampaignModel", timeout, interval);
-		if( campaignModelButton == null ) { logger.error(  Log.FAILED.createMessage( selenium.getTestName() , "Cannot open the Campaign Model DashBoard" ) ); return false; }	
+		logger.info( Log.SELECTING.createMessage( selenium.getTestName(), "to open the Traffic Generator DashBoard") );
+		trafficGeneratorButton.click();		
 		
-		logger.info( Log.SELECTING.createMessage( selenium.getTestName(), "for open the Campaign Model DashBoard") );
-		selenium.click( "id=gwt-debug-InputCMCampaignModel" );
-		*/
+		return true;
+		
+	}
+	
+	public static boolean setEvent( SeleniumWebDriver selenium, long timeout, long interval ) {
+					
+		JSONObject event = new JSONObject();
+		
+		try {
+			
+			event.put( "msisdn", "333669393643");
+			//event.put(  )
+		
+		} catch( JSONException e ) {}
+		
+		logger.info( Log.CHECKING.createMessage( selenium.getTestName(), "for id=gwt-debug-InputOPTrafficGenerator") );
+		
+		WebElement trafficGeneratorButton = SeleniumUtils.findForComponentDisplayed(selenium, SeleniumUtils.SearchBy.ID, "gwt-debug-InputOPTrafficGenerator", timeout, interval);
+		if( trafficGeneratorButton == null ) { logger.error(  Log.FAILED.createMessage( selenium.getTestName() , "Cannot open the Traffic Generator DashBoard" ) ); return false; }	
+		
+		logger.info( Log.SELECTING.createMessage( selenium.getTestName(), "to open the Traffic Generator DashBoard") );
+		trafficGeneratorButton.click();		
+		
+		// gwt-debug-BtnGUITrafficGeneratorEventExecute
+		// gwt-debug-InputGUITrafficGeneratorEventSubID
+		// gwt-debug-ListGUITrafficGeneratorEventClass
+		// gwt-debug-BtnGUITrafficGeneratorEventParametersAdd
+		// gwt-debug-InputGUITrafficGeneratorEventParameterName
+		// gwt-debug-InputGUITrafficGeneratorEventParameterValue
+		// gwt-debug-FormGUITrafficGeneratorEventParameterApply
+		// assertAlert  Event generated!
 		
 		return true;
 		
