562b93c39d2eb40124b47e8d4be74fb40b0249e7
Arcangelo Di Pasquale <arcangelo.dipasquale@lumatagroup.com>
2013-07-30 18:50:09 +0200

    Added BDR Class

---
+++ src/main/java/com/lumata/expression/operators/bdr/BDR.java
@@ -0,0 +1,164 @@
+package com.lumata.expression.operators.bdr;
+
+public class BDR {
+
+	private String format_id;
+	private String msisdn;
+	private String transaction_id;
+	private String current_date;
+	private String reward_name;
+	private String reward_qty;
+	private String operation_type;
+	private String module;
+	private String origin;
+	private String originator;
+	private String initial_reward_qty;
+	private String reward_limitation_cause;
+	
+	public BDR() {}
+	
+	public String getFormatID() {
+		
+		return this.format_id;
+		
+	}
+	
+	public String getMSISDN() {
+		
+		return this.msisdn;
+		
+	}
+
+	public String getTransactionID() {
+		
+		return this.transaction_id;
+		
+	}
+
+	public String getCurrentDate() {
+		
+		return this.current_date;
+		
+	}
+
+	public String getRewardName() {
+		
+		return this.reward_name;
+		
+	}
+
+	public String getRewardQty() {
+		
+		return this.reward_qty;
+		
+	}
+
+	public String getOperationType() {
+		
+		return this.operation_type;
+		
+	}
+
+	public String getModule() {
+		
+		return this.module;
+		
+	}
+
+	public String getOrigin() {
+		
+		return this.origin;
+		
+	}
+
+	public String getOriginator() {
+		
+		return this.originator;
+		
+	}
+
+	public String getInitialRewardQty() {
+		
+		return this.initial_reward_qty;
+		
+	}
+	
+	public String getRewardLimitationCause() {
+		
+		return this.reward_limitation_cause;
+		
+	}
+	
+	public void setFormatID( String format_id ) {
+		
+		this.format_id = format_id;
+		
+	}
+	
+	public void setMSISDN( String msisdn ) {
+		
+		this.msisdn = msisdn;
+		
+	}
+
+	public void setTransactionID( String transaction_id ) {
+		
+		this.transaction_id = transaction_id;
+		
+	}
+
+	public void setCurrentDate( String current_date ) {
+		
+		this.current_date = current_date;
+		
+	}
+
+	public void setRewardName( String reward_name ) {
+		
+		this.reward_name = reward_name;
+		
+	}
+
+	public void setRewardQty( String reward_qty ) {
+		
+		this.reward_qty = reward_qty;
+		
+	}
+
+	public void setOperationType( String operation_type ) {
+		
+		this.operation_type = operation_type;
+		
+	}
+
+	public void setModule( String module ) {
+		
+		this.module = module;
+		
+	}
+
+	public void setOrigin( String origin ) {
+		
+		this.origin = origin;
+		
+	}
+
+	public void setOriginator( String originator ) {
+		
+		this.originator = originator;
+		
+	}
+
+	public void setInitialRewardQty( String initial_reward_qty ) {
+		
+		this.initial_reward_qty = initial_reward_qty;
+		
+	}
+	
+	public void setRewardLimitationCause( String reward_limitation_cause ) {
+		
+		this.reward_limitation_cause = reward_limitation_cause;
+		
+	}
+
+}
