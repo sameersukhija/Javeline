d5795180e95ccc1ec235a4133fda1e636010d755
Arcangelo Di Pasquale <arcangelo.dipasquale@lumatagroup.com>
2013-09-20 12:46:06 +0200

    Added some testcases to test integration between Jenkins and Testlink

---
+++ src/test/java/com/lumata/expression/operators/testing/jenkins/CreateCampagne.java
@@ -0,0 +1,15 @@
+package com.lumata.expression.operators.testing.jenkins;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class CreateCampagne {
+
+	@Test
+	public void create_campagne () {
+		
+		Assert.assertTrue( true );
+		
+	}
+	
+}
---
+++ src/test/java/com/lumata/expression/operators/testing/jenkins/CreateSubscriber.java
@@ -0,0 +1,15 @@
+package com.lumata.expression.operators.testing.jenkins;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class CreateSubscriber {
+
+	@Test
+	public void create_subscriber () {
+		
+		Assert.assertTrue( true );
+		
+	}
+	
+}
