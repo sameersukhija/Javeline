62931a1e19f66ea6c0b1f41d11a1058a74de53fe
Arcangelo Di Pasquale <arcangelo.dipasquale@lumatagroup.com>
2013-09-20 10:39:12 +0200

    Added Login testcase to test integration between Jenkins and Testlink

---
+++ src/test/java/com/lumata/expression/operators/testing/jenkins/Login.java
@@ -0,0 +1,15 @@
+package com.lumata.expression.operators.testing.jenkins;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class Login {
+
+	@Test
+	public void login () {
+		
+		Assert.assertTrue( true );
+		
+	}
+	
+}
