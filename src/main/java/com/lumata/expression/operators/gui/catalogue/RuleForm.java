b3162e2c7b84c3f4378f45bc4cdc1ed7ca444864
alberto.colombo <alberto.colombo@lumatagroup.com>
2014-02-17 16:25:18 +0100

    add rule configuration

---
+++ src/main/java/com/lumata/expression/operators/gui/catalogue/RuleForm.java
@@ -0,0 +1,224 @@
+package com.lumata.expression.operators.gui.catalogue;
+
+import org.json.JSONObject;
+import org.openqa.selenium.WebElement;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.lumata.common.testing.log.Log;
+import com.lumata.common.testing.selenium.SeleniumUtils;
+import com.lumata.common.testing.selenium.SeleniumWebDriver;
+import com.lumata.expression.operators.gui.common.AngularFrame;
+import com.lumata.expression.operators.json.catalogue.RuleCfg;
+
+public class RuleForm {
+
+	private static final Logger logger = LoggerFactory.getLogger(RuleForm.class);
+
+	public enum RuleErrorAction {
+
+		RULE_ALREADY_EXISTS;
+
+	};
+
+	public enum RuleActionType {
+
+		RETURN_ERROR, ABORT, ADD_TIMESTAMP_TO_RULE_NAME;
+
+	};
+
+	public static boolean open(SeleniumWebDriver selenium, long timeout, long interval) {
+
+		return OfferOptimisationForm.open(selenium, OfferOptimisationForm.OfferOptimisationSection.RULES, timeout, interval);
+
+	}
+
+	public static boolean addRule(SeleniumWebDriver selenium, RuleCfg rule, long timeout, long interval) {
+
+		logger.info(Log.CHECKING.createMessage(selenium.getTestName(), "for link=Add"));
+		WebElement addRuleBtn = SeleniumUtils.findForComponentDisplayed(selenium, SeleniumUtils.SearchBy.LINK, "Add", timeout, interval);
+		if (addRuleBtn == null) {
+			return false;
+		}
+		addRuleBtn.click();
+
+		logger.info(Log.CHECKING.createMessage(selenium.getTestName(), "for name = name"));
+		WebElement ruleName = SeleniumUtils.findForComponentDisplayed(selenium, SeleniumUtils.SearchBy.NAME, "name", timeout, interval);
+		if (ruleName == null) {
+			return false;
+		}
+		ruleName.sendKeys(rule.getName());
+
+		//TODO: 
+		//		logger.info( Log.CHECKING.createMessage( selenium.getTestName(), "for name = Description") );		
+		//		WebElement ruleDesc = SeleniumUtils.findForComponentDisplayed(selenium, SeleniumUtils.SearchBy.NAME, "description", timeout, interval);
+		//		if( ruleDesc == null ) { return false; }			
+		//		ruleDesc.sendKeys(rule.getDescription());	
+
+		WebElement ruleTokenTypeName = SeleniumUtils.findForComponentDisplayed(selenium, SeleniumUtils.SearchBy.NAME, "tokenType", timeout, interval);
+		if (ruleTokenTypeName == null) {
+			return false;
+		}
+		selenium.select("name=tokenType", "label=" + rule.getTokenTypeName());
+
+		WebElement ruleAlgorithmName = SeleniumUtils.findForComponentDisplayed(selenium, SeleniumUtils.SearchBy.NAME, "algorithm", timeout, interval);
+		if (ruleAlgorithmName == null) {
+			return false;
+		}
+		selenium.select("name=algorithm", "label=" + rule.getOfferSelectionAlgorithmId());
+
+		if (rule.getKeepOffersConsistent()) {
+			logger.info(Log.CHECKING.createMessage(selenium.getTestName(), "for id = keepOffersConsistent-1"));
+			WebElement keepOffersConsistent1 = SeleniumUtils.findForComponentDisplayed(selenium, SeleniumUtils.SearchBy.ID, "keepOffersConsistent-1", timeout, interval);
+			if (keepOffersConsistent1 == null) {
+				return false;
+			}
+			keepOffersConsistent1.click();
+		} else {
+			logger.info(Log.CHECKING.createMessage(selenium.getTestName(), "for id = keepOffersConsistent-0"));
+			WebElement keepOffersConsistent0 = SeleniumUtils.findForComponentDisplayed(selenium, SeleniumUtils.SearchBy.ID, "keepOffersConsistent-0", timeout, interval);
+			if (keepOffersConsistent0 == null) {
+				return false;
+			}
+			keepOffersConsistent0.click();
+
+		}
+
+		if (rule.getPreviousOffersDrawnIncluded()) {
+			logger.info(Log.CHECKING.createMessage(selenium.getTestName(), "for id = previousOffersDrawnIncluded-1"));
+			WebElement previousOffersDrawnIncluded1 = SeleniumUtils.findForComponentDisplayed(selenium, SeleniumUtils.SearchBy.ID, "previousOffersDrawnIncluded-1", timeout, interval);
+			if (previousOffersDrawnIncluded1 == null) {
+				return false;
+			}
+			previousOffersDrawnIncluded1.click();
+		} else {
+			logger.info(Log.CHECKING.createMessage(selenium.getTestName(), "for id = previousOffersDrawnIncluded-0"));
+			WebElement previousOffersDrawnIncluded0 = SeleniumUtils.findForComponentDisplayed(selenium, SeleniumUtils.SearchBy.ID, "previousOffersDrawnIncluded-0", timeout, interval);
+			if (previousOffersDrawnIncluded0 == null) {
+				return false;
+			}
+			previousOffersDrawnIncluded0.click();
+		}
+
+		if (rule.getDuplicatedOfferWithinSingleDrawEnabled()) {
+			logger.info(Log.CHECKING.createMessage(selenium.getTestName(), "for id = duplicatedOfferWithinSingleDrawEnabled-1"));
+			WebElement duplicatedOfferWithinSingleDrawEnabled1 = SeleniumUtils.findForComponentDisplayed(selenium, SeleniumUtils.SearchBy.ID, "duplicatedOfferWithinSingleDrawEnabled-1",
+					timeout, interval);
+			if (duplicatedOfferWithinSingleDrawEnabled1 == null) {
+				return false;
+			}
+			duplicatedOfferWithinSingleDrawEnabled1.click();
+		} else {
+			logger.info(Log.CHECKING.createMessage(selenium.getTestName(), "for id = duplicatedOfferWithinSingleDrawEnabled-0"));
+			WebElement duplicatedOfferWithinSingleDrawEnabled0 = SeleniumUtils.findForComponentDisplayed(selenium, SeleniumUtils.SearchBy.ID, "duplicatedOfferWithinSingleDrawEnabled-0",
+					timeout, interval);
+			if (duplicatedOfferWithinSingleDrawEnabled0 == null) {
+				return false;
+			}
+			duplicatedOfferWithinSingleDrawEnabled0.click();
+		}
+
+		if (rule.getNumOfOffersToDrawUnlimited()) {
+			logger.info(Log.CHECKING.createMessage(selenium.getTestName(), "for id = numOfOffersToDrawUnlimited-1"));
+			WebElement numOfOffersToDrawUnlimited1 = SeleniumUtils.findForComponentDisplayed(selenium, SeleniumUtils.SearchBy.ID, "numOfOffersToDrawUnlimited-1", timeout, interval);
+			if (numOfOffersToDrawUnlimited1 == null) {
+				return false;
+			}
+			numOfOffersToDrawUnlimited1.click();
+		} else {
+			logger.info(Log.CHECKING.createMessage(selenium.getTestName(), "for id = numOfOffersToDrawUnlimited-0"));
+			WebElement numOfOffersToDrawUnlimited0 = SeleniumUtils.findForComponentDisplayed(selenium, SeleniumUtils.SearchBy.ID, "numOfOffersToDrawUnlimited-0", timeout, interval);
+			if (numOfOffersToDrawUnlimited0 == null) {
+				return false;
+			}
+			numOfOffersToDrawUnlimited0.click();
+		}
+		System.out.println("---------------------------->1");
+		logger.info(Log.CHECKING.createMessage(selenium.getTestName(), "for name = btn-add"));
+		WebElement ruleSave = SeleniumUtils.findForComponentDisplayed(selenium, SeleniumUtils.SearchBy.NAME, "btn-add", timeout, interval);
+		System.out.println("---------------------------->3");
+		if (ruleSave == null) {
+			System.out.println("---------------------------->3");
+			return false;
+		}
+		System.out.println("---------------------------->4");
+		ruleSave.click();
+		System.out.println("---------------------------->5");
+		return manageErrorAction(selenium, rule, timeout, interval);
+
+	}
+
+	public static boolean manageErrorAction(SeleniumWebDriver selenium, RuleCfg rule, long timeout, long interval) {
+
+		logger.info(Log.CHECKING.createMessage(selenium.getTestName(), "for error message"));
+
+		AngularFrame.close(selenium, timeout, interval);
+
+		WebElement btnMessageError = SeleniumUtils.findForComponentDisplayed(selenium, SeleniumUtils.SearchBy.XPATH, "//div[@class='gwt-DialogBox errorDialog']//button", 2000, 100);
+
+		if (btnMessageError != null) {
+
+			btnMessageError.click();
+
+			if (!AngularFrame.open(selenium, timeout, interval)) {
+				return false;
+			}
+
+			JSONObject error_actions = rule.getErrorActions();
+
+			if (error_actions == null) {
+
+				logger.error(Log.FAILED.createMessage(selenium.getTestName(), "Cannot add a new rule( Wrong json configuration )"));
+
+				return false;
+
+			} else {
+
+				try {
+
+					switch (RuleActionType.valueOf(error_actions.getString(RuleErrorAction.RULE_ALREADY_EXISTS.name()))) {
+
+					case RETURN_ERROR: {
+
+						logger.error(Log.FAILED.createMessage(selenium.getTestName(), "Cannot add a new rule ( rule name already exist )"));
+
+						return false;
+
+					}
+					case ADD_TIMESTAMP_TO_RULE_NAME: {
+
+						//TODO							
+
+						return true;
+
+					}
+					case ABORT: {
+
+						logger.info(Log.CHECKING.createMessage(selenium.getTestName(), "for name=btn-cancel"));
+
+						WebElement tokenTypeCancel = SeleniumUtils.findForComponentDisplayed(selenium, SeleniumUtils.SearchBy.XPATH, "html/body/div[1]/div[2]/div/div/div[2]/a[1]", timeout,
+								interval);
+						if (tokenTypeCancel == null) {
+							logger.error(Log.FAILED.createMessage(selenium.getTestName(), "Abort token type creation"));
+							return false;
+						}
+						tokenTypeCancel.click();
+
+						return true;
+
+					}
+
+					}
+
+				} catch (Exception e) {
+				}
+
+			}
+
+		}
+
+		return AngularFrame.open(selenium, timeout, interval);
+
+	}
+
+}
--- src/main/java/com/lumata/expression/operators/json/catalogue/RuleCfg.java
+++ src/main/java/com/lumata/expression/operators/json/catalogue/RuleCfg.java
@@ -1,7 +1,6 @@
 package com.lumata.expression.operators.json.catalogue;
 
 import java.util.ArrayList;
-import java.util.Iterator;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -15,8 +14,6 @@ import com.lumata.common.testing.io.IOFileUtils.IOLoadingType;
 import com.lumata.common.testing.io.JSONUtils;
 import com.lumata.common.testing.validating.Format;
 import com.lumata.expression.operators.exceptions.RuleException;
-import com.lumata.expression.operators.exceptions.TokenTypeException;
-
 
 /**
  * @author <a href="mailto:arcangelo.dipasquale@lumatagroup.com">Arcangelo Di Pasquale</a>
@@ -24,324 +21,387 @@ import com.lumata.expression.operators.exceptions.TokenTypeException;
  */
 public class RuleCfg {
 
-	private static final  Logger logger = LoggerFactory.getLogger( RuleCfg.class );
-	
-	public enum RuleValidity { seconds, minutes, hours, days }
-	
+	private static final Logger logger = LoggerFactory.getLogger(RuleCfg.class);
+
+	public enum RuleValidity {
+		seconds, minutes, hours, days
+	}
+
 	private JSONObject rlCfg;
-	
-	public RuleCfg( JSONObject rlCfg ) {
-		
+
+	public RuleCfg(JSONObject rlCfg) {
+
 		this.rlCfg = rlCfg;
-				
+
 	}
-	
-	public RuleCfg( String rule, IOLoadingType loadingType ) throws RuleException {
-		
+
+	public RuleCfg(String rule, IOLoadingType loadingType) throws RuleException {
+
 		try {
-			
-			switch( loadingType ) {
-			
-				case FILE: { this.rlCfg = JSONUtils.loadJSONFile( rule.toLowerCase() + Format.JSON_EXTENSION ); break; }
-				case RESOURCE: { this.rlCfg = JSONUtils.loadJSONResource( rule.toLowerCase() + Format.JSON_EXTENSION ); break;  }
-				default: throw new RuleException( "You cannot load an Campaign from resources different by FILE or RESOURCE" );
-			
-			}		
-									
-		} catch( JSONSException e ) {
-			
-			logger.error( e.getMessage(), e );
-			
-			throw new RuleException( e.getMessage(), e );
-			
-		} catch( IOFileException e ) {			
-			
-			logger.error( e.getMessage(), e );
-			
-			throw new RuleException( e.getMessage(), e );
-			
-		} 			
-			
-	}
-	
-	public RuleCfg( String folder, String rule, IOLoadingType loadingType ) throws RuleException {
-		
-		try {
-			
-			switch( loadingType ) {
-			
-				case FILE: { this.rlCfg = JSONUtils.loadJSONFile( folder, rule.toLowerCase() + Format.JSON_EXTENSION ); break; }
-				case RESOURCE: { this.rlCfg = JSONUtils.loadJSONResource( folder, rule.toLowerCase() + Format.JSON_EXTENSION ); break;  }
-				default: throw new RuleException( "You cannot load an Campaign from resources different by FILE or RESOURCE" );
-			
+
+			switch (loadingType) {
+
+			case FILE: {
+				this.rlCfg = JSONUtils.loadJSONFile(rule.toLowerCase() + Format.JSON_EXTENSION);
+				break;
 			}
-									
-		} catch( JSONSException e ) {
-			
-			logger.error( e.getMessage(), e );
-			
-			throw new RuleException( e.getMessage(), e );
-			
-		} catch( IOFileException e ) {			
-			
-			logger.error( e.getMessage(), e );
-			
-			throw new RuleException( e.getMessage(), e );
-			
-		} 			
-			
+			case RESOURCE: {
+				this.rlCfg = JSONUtils.loadJSONResource(rule.toLowerCase() + Format.JSON_EXTENSION);
+				break;
+			}
+			default:
+				throw new RuleException("You cannot load an Campaign from resources different by FILE or RESOURCE");
+
+			}
+
+		} catch (JSONSException e) {
+
+			logger.error(e.getMessage(), e);
+
+			throw new RuleException(e.getMessage(), e);
+
+		} catch (IOFileException e) {
+
+			logger.error(e.getMessage(), e);
+
+			throw new RuleException(e.getMessage(), e);
+
+		}
+
 	}
-	
-	public static ArrayList<RuleCfg> createTokenTypeList(  String folder, String path, IOLoadingType loadingType ) throws RuleException {
-		
-		ArrayList<RuleCfg> list = new ArrayList<RuleCfg>();
-		
+
+	public RuleCfg(String folder, String rule, IOLoadingType loadingType) throws RuleException {
+
 		try {
-			
+
+			switch (loadingType) {
+
+			case FILE: {
+				this.rlCfg = JSONUtils.loadJSONFile(folder, rule.toLowerCase() + Format.JSON_EXTENSION);
+				break;
+			}
+			case RESOURCE: {
+				this.rlCfg = JSONUtils.loadJSONResource(folder, rule.toLowerCase() + Format.JSON_EXTENSION);
+				break;
+			}
+			default:
+				throw new RuleException("You cannot load an Campaign from resources different by FILE or RESOURCE");
+
+			}
+
+		} catch (JSONSException e) {
+
+			logger.error(e.getMessage(), e);
+
+			throw new RuleException(e.getMessage(), e);
+
+		} catch (IOFileException e) {
+
+			logger.error(e.getMessage(), e);
+
+			throw new RuleException(e.getMessage(), e);
+
+		}
+
+	}
+
+	public static ArrayList<RuleCfg> createRuleList(String folder, String path, IOLoadingType loadingType) throws RuleException {
+
+		ArrayList<RuleCfg> finalList = new ArrayList<RuleCfg>();
+
+		try {
+
 			JSONObject rule_list;
-			
-			switch( loadingType ) {
-			
-				case FILE: { rule_list = JSONUtils.loadJSONFile( folder, path.toLowerCase() + Format.JSON_EXTENSION ); break; }
-				case RESOURCE: { rule_list = JSONUtils.loadJSONResource( folder, path.toLowerCase() + Format.JSON_EXTENSION ); break;  }
-				default: throw new RuleException( "You cannot load an Campaign from resources different by FILE or RESOURCE" );
-			
+
+			switch (loadingType) {
+
+			case FILE: {
+				rule_list = JSONUtils.loadJSONFile(folder, path.toLowerCase() + Format.JSON_EXTENSION);
+				break;
 			}
-			
-			@SuppressWarnings("unchecked")
-			Iterator<String> rules = rule_list.keys();
-			while( rules.hasNext() ) {
-				
-				String rule = (String)rules.next();
-				
-				list.add( new RuleCfg( rule_list.getJSONObject( rule ) ) );
-												
+			case RESOURCE: {
+				rule_list = JSONUtils.loadJSONResource(folder, path.toLowerCase() + Format.JSON_EXTENSION);
+				break;
 			}
-									
+			default:
+				throw new RuleException("You cannot load an Campaign from resources different by FILE or RESOURCE");
+
+			}
+
+			JSONArray ruleList = rule_list.getJSONArray("rule_list");
+
+			for (int i = 0; i < ruleList.length(); i++) {
+
+				finalList.add(new RuleCfg(ruleList.getJSONObject(i)));
+
+			}
+
 		} catch (JSONException e) {
-			
-			logger.error( e.getMessage(), e );
-			
-			throw new RuleException( e.getMessage(), e );
-		
-		} catch( JSONSException e ) {
-			
-			logger.error( e.getMessage(), e );
-			
-			throw new RuleException( e.getMessage(), e );
-			
-		} catch( IOFileException e ) {			
-			
-			logger.error( e.getMessage(), e );
-			
-			throw new RuleException( e.getMessage(), e );
-			
-		} 
-		
-		return list;
-		
+
+			logger.error(e.getMessage(), e);
+
+			throw new RuleException(e.getMessage(), e);
+
+		} catch (JSONSException e) {
+
+			logger.error(e.getMessage(), e);
+
+			throw new RuleException(e.getMessage(), e);
+
+		} catch (IOFileException e) {
+
+			logger.error(e.getMessage(), e);
+
+			throw new RuleException(e.getMessage(), e);
+
+		}
+
+		return finalList;
+
 	}
-	
+
 	public String getName() {
-		
-		try {
-			
-			if( !rlCfg.isNull("name") ) { return rlCfg.getString("name"); }
-		
-		} catch( Exception e ) {
 
-			logger.error( e.getMessage(), e );
-			
+		try {
+
+			if (!rlCfg.isNull("name")) {
+				return rlCfg.getString("name");
+			}
+
+		} catch (Exception e) {
+
+			logger.error(e.getMessage(), e);
+
 		}
-		
+
 		return null;
-		
+
 	}
-	
+
 	public String getDescription() {
-		
-		try {
-			
-			if( !rlCfg.isNull("description") ) { return rlCfg.getString("description"); }
-		
-		} catch( Exception e ) {
 
-			logger.error( e.getMessage(), e );
-			
+		try {
+
+			if (!rlCfg.isNull("description")) {
+				return rlCfg.getString("description");
+			}
+
+		} catch (Exception e) {
+
+			logger.error(e.getMessage(), e);
+
 		}
-		
+
 		return null;
-		
-	}	
-	
+
+	}
+
 	public String getTokenTypeName() {
-		
-		try {
-			
-			if( !rlCfg.isNull("tokenTypeName") ) { return rlCfg.getString("tokenTypeName"); }
-		
-		} catch( Exception e ) {
 
-			logger.error( e.getMessage(), e );
-			
+		try {
+
+			if (!rlCfg.isNull("tokenTypeName")) {
+				return rlCfg.getString("tokenTypeName");
+			}
+
+		} catch (Exception e) {
+
+			logger.error(e.getMessage(), e);
+
 		}
-		
+
 		return null;
-		
+
 	}
 
 	public JSONArray getChannelNameList() {
-		
-		try {
-			
-			if( !rlCfg.isNull("channelNamesList") ) { return rlCfg.getJSONArray("channelNamesList"); }
-		
-		} catch( Exception e ) {
 
-			logger.error( e.getMessage(), e );
-			
+		try {
+
+			if (!rlCfg.isNull("channelNamesList")) {
+				return rlCfg.getJSONArray("channelNamesList");
+			}
+
+		} catch (Exception e) {
+
+			logger.error(e.getMessage(), e);
+
 		}
-		
+
 		return null;
-		
+
 	}
 
 	public String getOfferSelectionAlgorithmId() {
-		
-		try {
-			
-			if( !rlCfg.isNull("offerSelectionAlgorithmId") ) { return rlCfg.getString("offerSelectionAlgorithmId"); }
-		
-		} catch( Exception e ) {
 
-			logger.error( e.getMessage(), e );
-			
+		try {
+
+			if (!rlCfg.isNull("offerSelectionAlgorithmId")) {
+				return rlCfg.getString("offerSelectionAlgorithmId");
+			}
+
+		} catch (Exception e) {
+
+			logger.error(e.getMessage(), e);
+
 		}
-		
+
 		return null;
-		
+
 	}
 
 	public JSONArray getRequestorIdsList() {
-		
-		try {
-			
-			if( !rlCfg.isNull("requestorIdsList") ) { return rlCfg.getJSONArray("requestorIdsList"); }
-		
-		} catch( Exception e ) {
 
-			logger.error( e.getMessage(), e );
-			
+		try {
+
+			if (!rlCfg.isNull("requestorIdsList")) {
+				return rlCfg.getJSONArray("requestorIdsList");
+			}
+
+		} catch (Exception e) {
+
+			logger.error(e.getMessage(), e);
+
 		}
-		
+
 		return null;
-		
+
 	}
 
 	public Boolean getKeepOffersConsistent() {
-		
-		try {
-			
-			if( !rlCfg.isNull("keepOffersConsistent") ) { return rlCfg.getBoolean("keepOffersConsistent"); }
-		
-		} catch( Exception e ) {
 
-			logger.error( e.getMessage(), e );
-			
+		try {
+
+			if (!rlCfg.isNull("keepOffersConsistent")) {
+				return rlCfg.getBoolean("keepOffersConsistent");
+			}
+
+		} catch (Exception e) {
+
+			logger.error(e.getMessage(), e);
+
 		}
-		
+
 		return null;
-		
+
 	}
 
 	public Boolean getPreviousOffersDrawnIncluded() {
-		
-		try {
-			
-			if( !rlCfg.isNull("previousOffersDrawnIncluded") ) { return rlCfg.getBoolean("previousOffersDrawnIncluded"); }
-		
-		} catch( Exception e ) {
 
-			logger.error( e.getMessage(), e );
-			
+		try {
+
+			if (!rlCfg.isNull("previousOffersDrawnIncluded")) {
+				return rlCfg.getBoolean("previousOffersDrawnIncluded");
+			}
+
+		} catch (Exception e) {
+
+			logger.error(e.getMessage(), e);
+
 		}
-		
+
 		return null;
-		
+
 	}
 
 	public Boolean getDuplicatedOfferWithinSingleDrawEnabled() {
-		
-		try {
-			
-			if( !rlCfg.isNull("duplicatedOfferWithinSingleDrawEnabled") ) { return rlCfg.getBoolean("duplicatedOfferWithinSingleDrawEnabled"); }
-		
-		} catch( Exception e ) {
 
-			logger.error( e.getMessage(), e );
-			
+		try {
+
+			if (!rlCfg.isNull("duplicatedOfferWithinSingleDrawEnabled")) {
+				return rlCfg.getBoolean("duplicatedOfferWithinSingleDrawEnabled");
+			}
+
+		} catch (Exception e) {
+
+			logger.error(e.getMessage(), e);
+
 		}
-		
+
 		return null;
-		
+
 	}
 
 	public Boolean getNumOfOffersToDrawUnlimited() {
-		
-		try {
-			
-			if( !rlCfg.isNull("numOfOffersToDrawUnlimited") ) { return rlCfg.getBoolean("numOfOffersToDrawUnlimited"); }
-		
-		} catch( Exception e ) {
 
-			logger.error( e.getMessage(), e );
-			
+		try {
+
+			if (!rlCfg.isNull("numOfOffersToDrawUnlimited")) {
+				return rlCfg.getBoolean("numOfOffersToDrawUnlimited");
+			}
+
+		} catch (Exception e) {
+
+			logger.error(e.getMessage(), e);
+
 		}
-		
+
 		return null;
-		
+
 	}
 
 	public Integer getNumOfOffersToDraw() {
-		
-		try {
-			
-			if( !rlCfg.isNull("numOfOffersToDraw") ) { return rlCfg.getInt("numOfOffersToDraw"); }
-		
-		} catch( Exception e ) {
 
-			logger.error( e.getMessage(), e );
-			
+		try {
+
+			if (!rlCfg.isNull("numOfOffersToDraw")) {
+				return rlCfg.getInt("numOfOffersToDraw");
+			}
+
+		} catch (Exception e) {
+
+			logger.error(e.getMessage(), e);
+
 		}
-		
+
 		return null;
-		
+
 	}
 
 	public Integer getAllocationExpiration() {
-		
-		try {
-			
-			if( !rlCfg.isNull("allocationExpiration") ) { return rlCfg.getInt("allocationExpiration"); }
-		
-		} catch( Exception e ) {
 
-			logger.error( e.getMessage(), e );
-			
+		try {
+
+			if (!rlCfg.isNull("allocationExpiration")) {
+				return rlCfg.getInt("allocationExpiration");
+			}
+
+		} catch (Exception e) {
+
+			logger.error(e.getMessage(), e);
+
 		}
-		
+
 		return null;
-		
+
 	}
 
 	public String getAllocationExpirationUnit() {
+
+		try {
+
+			if (!rlCfg.isNull("allocationExpirationUnit")) {
+				return rlCfg.getString("allocationExpirationUnit");
+			}
+
+		} catch (Exception e) {
+
+			logger.error(e.getMessage(), e);
+
+		}
+
+		return null;
+
+	}
+
+	public JSONObject getErrorActions() {
 		
 		try {
-			
-			if( !rlCfg.isNull("allocationExpirationUnit") ) { return rlCfg.getString("allocationExpirationUnit"); }
 		
-		} catch( Exception e ) {
-
+			if( !rlCfg.isNull("error_actions") ) { return rlCfg.getJSONObject("error_actions"); }
+		
+		} catch( JSONException e ) {
+			
 			logger.error( e.getMessage(), e );
 			
 		}
@@ -349,5 +409,5 @@ public class RuleCfg {
 		return null;
 		
 	}
-	
+
 }
