677b3abff599179be9e8e629f395e51fb13a2c6a
Davide Rognoni <davide.rognoni@lumatagroup.com>
2014-03-13 17:06:20 +0100

    XPATH more clear --> //*[text()='BadgesProgName']/../../../../../../../../../..//*[@title='Edit']

--- src/main/java/com/lumata/expression/operators/gui/loyalty/LoyaltyCreationForm.java
+++ src/main/java/com/lumata/expression/operators/gui/loyalty/LoyaltyCreationForm.java
@@ -133,7 +133,8 @@ public class LoyaltyCreationForm {
 				//"html/body/table[2]/tbody/tr/td/table/tbody/tr[2]/td/div/div[2]/table/tbody/tr[2]/td/div/div[1]/table/tbody/tr[2]/td/div/div/table/tbody/tr[3]/td/table/tbody/tr[2]/td/div/table/tbody/tr/td/table/tbody/tr[6]/td[3]/table/tbody/tr/td[3]/button", timeout, interval);
 				//"html/body/table[2]/tbody/tr/td/table/tbody/tr[2]/td/div/div[2]/table/tbody/tr[2]/td/div/div[1]/table/tbody/tr[2]/td/div/div/table/tbody/tr[1]/td/table/tbody/tr[2]/td/div/table/tbody/tr/td/table/tbody/tr[6]/td[3]/table/tbody/tr/td[3]/button", timeout, interval);
 		  		//"//*[text()='BadgesProgName']/../../../../../../../../../../td/table/tbody/tr[2]/td/div/table/tbody/tr/td/table/tbody/tr[6]/td[3]/table/tbody/tr/td[3]/button", timeout, interval);
-		  		  "//*[text()='BadgesProgName']/../../../../../../../../../..//*/td[3]/button", timeout, interval);
+		  		//"//*[text()='BadgesProgName']/../../../../../../../../../..//*/td[3]/button", timeout, interval);
+		  		  "//*[text()='BadgesProgName']/../../../../../../../../../..//*[@title='Edit']", timeout, interval);
 		if (editProgram == null) { return false; }
 		editProgram.click();
 
