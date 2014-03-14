a716693a9ac018b31a62f131922858ff5acfcf7b
Davide Rognoni <davide.rognoni@lumatagroup.com>
2014-03-14 16:55:02 +0100

    Used abstract "Form".

--- src/main/java/com/lumata/expression/operators/gui/loyalty/LoyaltyCreationForm.java
+++ src/main/java/com/lumata/expression/operators/gui/loyalty/LoyaltyCreationForm.java
@@ -3,32 +3,25 @@ package com.lumata.expression.operators.gui.loyalty;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.support.ui.Select;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import com.lumata.common.testing.database.Mysql;
 import com.lumata.common.testing.log.Log;
-import com.lumata.common.testing.selenium.SeleniumUtils;
 import com.lumata.common.testing.selenium.SeleniumWebDriver;
 import com.lumata.expression.operators.gui.campaigns.CampaignModelForm;
 import com.lumata.expression.operators.gui.catalogue.LoyaltyForm;
+import com.lumata.expression.operators.gui.common.Form;
 
-public class LoyaltyCreationForm {
+public class LoyaltyCreationForm extends Form {
 
 	private static final Logger logger = LoggerFactory.getLogger(CampaignModelForm.class);
 	
-	private SeleniumWebDriver selenium;
 	private Mysql mysql;
-	private long timeout;
-	private long interval;
 	
 	public LoyaltyCreationForm(SeleniumWebDriver selenium, Mysql mysql, long timeout, long interval) {
-		this.selenium = selenium;
+		super(selenium, timeout, interval);
 		this.mysql = mysql;
-		this.timeout = timeout;
-		this.interval = interval;
 	}
 	
 	public boolean open() {
@@ -92,49 +85,6 @@ public class LoyaltyCreationForm {
 			return false;
 		}
 	}
-	
-	public boolean click(String forName, String xpath) {
-		logger.info(Log.CHECKING.createMessage(selenium.getTestName(), "for " + forName));
-		WebElement we = SeleniumUtils.findForComponentDisplayed(selenium, SeleniumUtils.SearchBy.XPATH,
-				xpath, timeout, interval);
-		if (we == null) { return false; }
-		we.click();		
-		
-		return true;
-	}
-
-	public boolean sendKeys(String forName, String xpath, String text) {
-		logger.info(Log.CHECKING.createMessage(selenium.getTestName(), "for " + forName));
-		WebElement we = SeleniumUtils.findForComponentDisplayed(selenium, SeleniumUtils.SearchBy.XPATH,
-				xpath, timeout, interval);
-		if (we == null) { return false; }
-		we.sendKeys(text);
-		
-		return true;
-	}
-
-	public boolean selectByVisibleText(String forName, String xpath, String text) {
-		logger.info(Log.CHECKING.createMessage(selenium.getTestName(), "for " + forName));
-		WebElement selectUnitRecharge = SeleniumUtils.findForComponentDisplayed(selenium, SeleniumUtils.SearchBy.XPATH,
-				xpath, timeout, interval);
-		if (selectUnitRecharge == null) { return false; }
-		Select select = new Select(selectUnitRecharge);
-		select.selectByVisibleText(text);
-		
-		return true;
-	}
-	
-	/*public SeleniumWebDriver getSelenium() {
-		return selenium;
-	}
-
-	public long getTimeout() {
-		return timeout;
-	}
-
-	public long getInterval() {
-		return interval;
-	}*/
 
 	public static void main(String[] args) throws Exception {
 		
