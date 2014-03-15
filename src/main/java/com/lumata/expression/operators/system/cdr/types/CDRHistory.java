207d56c7a36733850f979b1c0eda3a9317f05249
Arcangelo Di Pasquale <arcangelo.dipasquale@lumatagroup.com>
2014-03-15 08:14:32 +0100

    Modified some CDR type classes

---
+++ src/main/java/com/lumata/expression/operators/system/cdr/types/CDRHistory.java
@@ -0,0 +1,57 @@
+package com.lumata.expression.operators.system.cdr.types;
+
+import com.lumata.expression.operators.system.cdr.CDR;
+import com.lumata.expression.operators.system.cdr.annotations.*;
+import com.lumata.expression.operators.exceptions.CDRException;
+
+public class CDRHistory extends CDR { 
+
+	private final int FIELDS = 1;
+
+	public CDRHistory() {
+		super();
+	} 
+
+	public int getFieldsCount() {
+		return this.FIELDS;
+	}
+
+	@Msisdn( position = 0 )
+	public String getMsisdn() throws CDRException  {
+		return super.getMsisdn();
+	}
+
+	public void setMsisdnOptions( final Integer prefix, final Integer length ) throws CDRException  {
+		super.setMsisdnOptions( prefix, length );
+	}
+
+	public void setMsisdnStrategyFixed( final Long value ) throws CDRException  {
+		super.setMsisdnStrategyFixed( value );
+	}
+
+	public void setMsisdnStrategyIncrement( final Long value, final Integer increment ) throws CDRException  {
+		super.setMsisdnStrategyIncrement( value, increment );
+	}
+
+	public void setMsisdnStrategyRandom( final Long min_value, final Long max_value ) throws CDRException  {
+		super.setMsisdnStrategyRandom( min_value, max_value );
+	}
+
+	public void cleanMsisdn()  {
+		super.cleanMsisdn() ;
+	}
+
+	public void cleanMsisdnOptions()  {
+		super.cleanMsisdnOptions() ;
+	}
+
+	public void cleanMsisdnStrategyIncrement()  {
+		super.cleanMsisdnStrategyIncrement() ;
+	}
+
+	public void cleanMsisdnStrategyRandom()  {
+		super.cleanMsisdnStrategyRandom() ;
+	}
+
+
+ }
