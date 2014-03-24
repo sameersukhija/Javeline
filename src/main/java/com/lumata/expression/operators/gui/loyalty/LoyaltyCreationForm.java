034240ca3c553f51daa02eeafd9912b89c74be17
Davide Rognoni <davide.rognoni@lumatagroup.com>
2014-03-24 16:20:26 +0100

    .

--- src/main/java/com/lumata/expression/operators/gui/loyalty/LoyaltyCreationForm.java
+++ src/main/java/com/lumata/expression/operators/gui/loyalty/LoyaltyCreationForm.java
@@ -36,20 +36,25 @@ public class LoyaltyCreationForm extends Form {
 
 	public void create() throws Exception {
 		
-		clickFormat("addNewProgramPopup", "html/body/table[2]/tbody/tr/td/table/tbody/tr[2]/td/div/div[2]/table/tbody/tr[2]/td/div/div[2]/table/tbody/tr[2]/td/div/div/table//*[text()='%s']/../../../../../../..//*[@title='Add']",
+		clickFormat("addNewProgramPopup",
+			"html/body/table[2]/tbody/tr/td/table/tbody/tr[2]/td/div/div[2]/table/tbody/tr[2]/td/div/div[2]/table/tbody/tr[2]/td/div/div/table//*[text()='%s']/../../../../../../..//*[@title='Add']",
 			createCfg.getAccordionName());
 		
-		sendKeys("programNameInput", "html/body/div[5]/div/table/tbody/tr[2]/td[2]/div/table/tbody/tr/td/table/tbody/tr[1]/td/table/tbody/tr[1]/td[2]/input",
+		sendKeys("programNameInput",
+			"html/body/div[5]/div/table/tbody/tr[2]/td[2]/div/table/tbody/tr/td/table/tbody/tr[1]/td/table/tbody/tr[1]/td[2]/input",
 			createCfg.getProgramName());
 		
-		sendKeys("programDescInput", "html/body/div[5]/div/table/tbody/tr[2]/td[2]/div/table/tbody/tr/td/table/tbody/tr[1]/td/table/tbody/tr[2]/td[2]/input",
+		sendKeys("programDescInput",
+			"html/body/div[5]/div/table/tbody/tr[2]/td[2]/div/table/tbody/tr/td/table/tbody/tr[1]/td/table/tbody/tr[2]/td[2]/input",
 			createCfg.getProgramDesc());
 		
-		click("programSave", "html/body/div[5]/div/table//*[@title='Save']");
+		click("programSave",
+			"html/body/div[5]/div/table//*[@title='Save']");
 		
 		addBadgeTypeFromList(createCfg.getTypeNameList());
 		
-		click("badgeTypeClose", "html/body/div[5]/div/table//*[@title='Close']");
+		click("badgeTypeClose",
+			"html/body/div[5]/div/table//*[@title='Close']");
 	}
 	
 	public void manage() throws Exception {
@@ -57,15 +62,19 @@ public class LoyaltyCreationForm extends Form {
 		click("subSectionTab",
 			"html/body/table[2]/tbody/tr/td/table/tbody/tr[2]/td/div/div[2]/table/tbody/tr[1]/td/table//*[text()='Management']");
 		
-		clickFormat("editProgram", "//*[text()='%s']/../../../../../../../../../..//*[@title='Edit']",
+		clickFormat("editProgram",
+			"//*[text()='%s']/../../../../../../../../../..//*[@title='Edit']",
 			createCfg.getProgramName());
 		
-		click("addBadge", "html/body/div[5]/div/table//*[@title='Add']");
+		click("addBadge",
+			"html/body/div[5]/div/table//*[@title='Add']");
 		
-		sendKeys("badgeDefinitionName", "html/body/div[7]/div/table/tbody/tr[2]/td[2]/div/table/tbody/tr/td/table/tbody/tr[1]/td/table/tbody/tr[2]/td/table/tbody/tr/td/table/tbody/tr[1]/td[2]/input",
+		sendKeys("badgeDefinitionName",
+			"html/body/div[7]/div/table/tbody/tr[2]/td[2]/div/table/tbody/tr/td/table/tbody/tr[1]/td/table/tbody/tr[2]/td/table/tbody/tr/td/table/tbody/tr[1]/td[2]/input",
 			manageCfg.getDefinitionName());
 		
-		sendKeys("badgeDefinitionDesc", "html/body/div[7]/div/table/tbody/tr[2]/td[2]/div/table/tbody/tr/td/table/tbody/tr[1]/td/table/tbody/tr[2]/td/table/tbody/tr/td/table/tbody/tr[2]/td[2]/input",
+		sendKeys("badgeDefinitionDesc",
+			"html/body/div[7]/div/table/tbody/tr[2]/td[2]/div/table/tbody/tr/td/table/tbody/tr[1]/td/table/tbody/tr[2]/td/table/tbody/tr/td/table/tbody/tr[2]/td[2]/input",
 			manageCfg.getDefinitionDescription());
 		
 		click("next", "html/body/div[7]/div/table//*[@title='Next']");
@@ -79,15 +88,18 @@ public class LoyaltyCreationForm extends Form {
 		click("eventType",
 			"html/body/div[7]/div/table/tbody/tr[2]/td[2]/div/table/tbody/tr/td/table/tbody/tr[1]/td/table/tbody/tr[2]/td/table/tbody/tr/td/table/tbody/tr[2]/td/table/tbody/tr/td/table/tbody/tr[2]/td[1]/div/table/tbody/tr/td");
 		
-		clickFormat("selectRevenue", "html/body/div[8]/div/table/tbody/tr[2]/td[2]/div/div/table//*[text()='%s']",
+		clickFormat("selectRevenue",
+			"html/body/div[8]/div/table/tbody/tr[2]/td[2]/div/div/table//*[text()='%s']",
 			manageCfg.getAwardedEventType());
 		
 		click("addAction", "//*[@id='gwt-debug-BtnCampaignModelCreationEAAdd']");
 		
-		selectByVisibleText("selectUnitRecharge", "//*[@id='gwt-debug-ListCampaignModelCreationEAUnit']",
+		selectByVisibleText("selectUnitRecharge",
+			"//*[@id='gwt-debug-ListCampaignModelCreationEAUnit']",
 			manageCfg.getAwardedActionUnit());
 		
-		sendKeys("points", "//*[@id='gwt-debug-TextCampaignModelCreationEAValue']",
+		sendKeys("points",
+			"//*[@id='gwt-debug-TextCampaignModelCreationEAValue']",
 			manageCfg.getAwardedActionPlus());
 		
 		click("saveBadge", "html/body/div[7]/div/table//*[@title='Save']");
