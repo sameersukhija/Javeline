2bdbc30eb67e17737360a9a62178cbac8412fbf8
Vincenzo Marrazzo <vincenzo.marrazzo@lumatagroup.com>
2014-05-23 18:15:24 +0200

    draft commodities json/form/test

--- src/main/java/com/lumata/e4o/gui/administrationmanager/CommoditiesForm.java
+++ src/main/java/com/lumata/e4o/gui/administrationmanager/CommoditiesForm.java
@@ -4,44 +4,69 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
+import org.openqa.selenium.Alert;
+import org.openqa.selenium.NoAlertPresentException;
 import org.openqa.selenium.StaleElementReferenceException;
 import org.openqa.selenium.WebElement;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import com.lumata.common.testing.exceptions.JSONSException;
+import com.lumata.common.testing.selenium.SeleniumUtils;
+import com.lumata.common.testing.selenium.SeleniumUtils.SearchBy;
 import com.lumata.common.testing.selenium.SeleniumWebDriver;
 import com.lumata.e4o.exceptions.FormException;
 import com.lumata.e4o.json.gui.administrationmanager.JSONCommodities;
 
 public class CommoditiesForm extends AdministrationForm {
 
+	/**
+	 * 
+	 */
 	private static final Logger logger = LoggerFactory.getLogger(CommoditiesForm.class);
 	
+	/**
+	 * 
+	 */
 	private JSONCommodities commoditiesCfg;
 	
+	/**
+	 * 
+	 * @param selenium
+	 * @param commoditiesCfg
+	 * @param timeout
+	 * @param interval
+	 */
 	public CommoditiesForm( SeleniumWebDriver selenium, JSONCommodities commoditiesCfg, long timeout, long interval ) {
 		
 		super(selenium, timeout, interval);
 		
 		this.commoditiesCfg = commoditiesCfg;
-		
 	}
 	
+	/**
+	 * 
+	 */
 	public CommoditiesForm open() throws FormException {
 		
 		super.open().clickId( "gwt-debug-Enter-Commodities" );
 		
-		return this;
-		
+		return this;	
 	}
 	
-	public CommoditiesForm addCommoditiesList() throws FormException {
+	/**
+	 * 
+	 * @return
+	 * @throws FormException
+	 * @throws JSONSException
+	 */
+	public CommoditiesForm addCommoditiesList() throws FormException, JSONSException {
 		
-		ArrayList<String> commodities = commoditiesCfg.list();
+		int numbCommodities = commoditiesCfg.getList().size();
 		
-		for( int commodityIndex = 0; commodityIndex < commodities.size(); commodityIndex++ ) {
+		for( int commodityIndex = 0; commodityIndex < numbCommodities; commodityIndex++ ) {
 			
-			commoditiesCfg.setCurrentCommodity( commodities.get( commodityIndex ) );
+			commoditiesCfg.setCurrentElementById(commodityIndex);
 			
 			clickId( "gwt-debug-Add Bonus" ).
 			createCommodity().
@@ -50,10 +75,15 @@ public class CommoditiesForm extends AdministrationForm {
 		}
 		
 		return this;
-		
 	}
 
-	public CommoditiesForm createCommodity() throws FormException {
+	/**
+	 * 
+	 * @return
+	 * @throws FormException
+	 * @throws JSONSException
+	 */
+	public CommoditiesForm createCommodity() throws FormException, JSONSException {
 					
 		selectById( "gwt-debug-Bonus Type", commoditiesCfg.getType() );
 
@@ -72,9 +102,38 @@ public class CommoditiesForm extends AdministrationForm {
 		return this;
 	}
 	
+	/**
+	 * 
+	 * @return
+	 * @throws FormException
+	 */
 	public CommoditiesForm saveCommodity() throws FormException {
 		
-		clickId( "gwt-debug-Bonus Save" );
+		if ( containsErrorElement() )
+			logger.error("Without click \"save\" panel is in error!");
+		
+		clickXPath( "//button[@title='Save']" );
+		
+		Boolean confirmed = Boolean.FALSE;
+
+		// error condition
+		//*[contains(@class,'errorBackground')]
+		
+		// key field
+		//*[@id='gwt-debug-Bonus Name']
+		
+		// key field already present
+//		"//div[text()='Bonus name already used']"
+		
+		confirmed = handleJavascriptAlertAcceptDismiss(true);
+
+		// in case no confirmation was executed, check element in error
+		if ( !confirmed && containsErrorElement() )
+			logger.error("After click \"save\" panel is in error!");
+		
+		/**
+		 * START REFACTORING - ERROR ACTIONS
+		 */
 		
 		try {
 		
@@ -89,6 +148,10 @@ public class CommoditiesForm extends AdministrationForm {
 			System.out.println( "button not visible" );
 			
 		}	
+		
+		/**
+		 * END REFACTORING
+		 */
 		
 		return this;
 		
@@ -112,12 +175,61 @@ public class CommoditiesForm extends AdministrationForm {
 	}
 	
 	/**
+	 * This method returns the presence of web component in error condition.
+	 * The error condition is at application level for the actual form.
+	 * (e.g. duplication of value, missing field) 
+	 * 
+	 * @return true if at least one element is in error
+	 * 
+	 * @throws FormException
+	 */
+	private Boolean containsErrorElement() throws FormException {
+		
+		Boolean resp = Boolean.TRUE;
+
+		searchByXPath("//div[@class='gwt-DialogBox']");
+		
+		// error condition
+		//*[contains(@class,'errorBackground')]
+		List<WebElement> element = SeleniumUtils.findListForComponentDisplayed(selenium, SearchBy.XPATH, lastWebElement, "//*[contains(@class,'errorBackground')]");
+		
+		if ( element.size() != 0 )
+			resp = true;
+		else
+			resp = false;
+		
+		return resp;
+	}
+	
+	/**
+	 * This method returns a list with <b>WebElement</b> in error condition.
+	 * The error condition is at application level for the actual form.
+	 * (e.g. duplication of value, missing field) 
+	 * 
+	 * @return a <b>List</b> of element in error condition.
+	 * 
+	 * @throws FormException
+	 */
+	private List<WebElement> getErrorElement() throws FormException {
+		
+		List<WebElement> resp = null;
+
+		searchByXPath("//div[@class='gwt-DialogBox']");
+		
+		// error condition
+		//*[contains(@class,'errorBackground')]
+		resp = SeleniumUtils.findListForComponentDisplayed(selenium, SearchBy.XPATH, lastWebElement, "//*[contains(@class,'errorBackground')]");
+		
+		return resp;
+	}
+	
+	/**
 	 * This method delete a list of "Commodities" into running system.
 	 * If input var-args is empty or null, this method deletes each commodities
 	 * 
 	 * @param CommoditiesNames
 	 * 
-	 * @return true if wanted campaign model are corrected removed
+	 * @return true if wanted commodities are corrected removed
 	 * 
 	 * @throws FormException 
 	 */
@@ -141,7 +253,7 @@ public class CommoditiesForm extends AdministrationForm {
 				commoditiesLabel.add(webElement.getText());
 		}
 
-		logger.debug("Campaign labels to be deleted : " + commoditiesLabel);
+		logger.debug("Commodities element to be deleted : " + commoditiesLabel);
 		
 		try {
 			
@@ -149,9 +261,11 @@ public class CommoditiesForm extends AdministrationForm {
 				
 				String singleRule = "//div[text()='"+cnName+"']//ancestor::tr[1]//*[@name='btn-delete']";
 
-				logger.debug("Try to delete \"Campaign Moldel \" + \""+cnName+"\".");
+				logger.debug("Try to delete \"Commodity\" with name + \""+cnName+"\".");
 				
 				clickXPath(singleRule);
+				
+				handleJavascriptAlertAcceptDismiss(true);
 			}
 			
 			resp = Boolean.TRUE;
@@ -164,6 +278,43 @@ public class CommoditiesForm extends AdministrationForm {
 		}
 
 		return resp;
+	}
+
+	/**
+	 * This method handles the popup warning displayed through Javascript.
+	 * 
+	 * The popup is the ones with Accept/Dismiss possible choice.
+	 * 
+	 * If input Boolean is TRUE will be pressed "Accept" condition otherwise "Dismiss". 
+	 * 
+	 * The returns Boolean describe if popup was displayed and pressed.
+	 */
+	private Boolean handleJavascriptAlertAcceptDismiss(Boolean accept) {
+		
+		Alert popupAlert = null;
+		Boolean pressed = null;
+		
+		try {
+			
+			popupAlert = selenium.getWrappedDriver().switchTo().alert();
+		    	
+			if ( popupAlert != null ) { 
+				
+				if ( accept )
+					popupAlert.accept();
+				else 
+					popupAlert.dismiss();
+				
+				pressed = Boolean.TRUE; 
+			}
+			
+		} catch (NoAlertPresentException e) {
+			
+			// nothing to do
+			pressed = Boolean.FALSE;
+		}
+		
+		return pressed;
 	}	
 	
 }
---
+++ src/test/java/com/lumata/e4o/gui/common/ParentUITestCase.java
@@ -0,0 +1,123 @@
+package com.lumata.e4o.gui.common;
+
+import org.testng.Reporter;
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeSuite;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Optional;
+import org.testng.annotations.Parameters;
+import org.testng.annotations.Test;
+
+import com.lumata.common.testing.exceptions.IOFileException;
+import com.lumata.common.testing.exceptions.JSONSException;
+import com.lumata.common.testing.exceptions.NetworkEnvironmentException;
+import com.lumata.common.testing.io.IOFileUtils.IOLoadingType;
+import com.lumata.common.testing.log.Log;
+import com.lumata.common.testing.selenium.SeleniumWebDriver;
+import com.lumata.common.testing.system.NetworkEnvironment;
+import com.lumata.common.testing.system.Server;
+import com.lumata.e4o.exceptions.FormException;
+import com.lumata.e4o.gui.security.Authorization;
+
+/**
+ * This is the parent class for test case for UI for E4O. It provides the follow facilities :
+ * 
+ * <li> handle the input environment configuration file
+ * <li> initialize Selenium driver according configuration
+ * <li> facilities like login and logout into UI
+ * 
+ * @author <a href="mailto:vincenzo.marrazzo@lumatagroup.com">Vincenzo Marrazzo</a>
+ */
+public class ParentUITestCase {
+	
+	/**
+	 * Print to standard output during execution
+	 */
+	protected static final Boolean PRINT2STDOUT__ = Boolean.TRUE;
+	
+	/**
+	 * Default resource path
+	 */
+	private static final String DEFAULT_RESOURCE_START_PATH__ = "input";
+	private static final String DEFAULT_LOADING_TYPE__ = "RESOURCE";
+	
+	protected final Integer TIMEOUT = 60000;
+	protected final Integer ATTEMPT_TIMEOUT = 50;
+	
+	protected NetworkEnvironment env = null;
+	protected Server gui = null;
+	protected SeleniumWebDriver seleniumWebDriver = null;
+	
+	private Authorization auth = null;
+	
+	/**
+	 * Used to retrieve configuration resource during execution
+	 */
+	protected String currentResourceStartPath;
+	
+	/* 	Initialize Environment */
+	@BeforeSuite
+	@Parameters({"browser", "environment", "loadingType", "resourceStartPath", "gui_server"})
+	public void setUp( 	@Optional("FIREFOX") String browser, 
+						@Optional("E4O_QA_NE") String environment, 
+						@Optional(DEFAULT_LOADING_TYPE__) String loadingType,
+						@Optional(DEFAULT_RESOURCE_START_PATH__) String resourceStartPath,
+						@Optional("actrule") String gui_server
+					) throws JSONSException, IOFileException, NetworkEnvironmentException {		
+		
+		currentResourceStartPath = resourceStartPath;
+	
+		Reporter.log("Starting path for looking \"resource\" will be -> " + currentResourceStartPath);
+		Reporter.log("Loading type for \"resource\" will be -> " + loadingType);
+		
+		Reporter.log( Log.LOADING.createMessage( "init" , "environment" ), PRINT2STDOUT__);
+	
+		String envPath = currentResourceStartPath + "/environments";
+		String envFile = environment;
+		
+		Reporter.log( "Init testing environment with reosurce file : ", PRINT2STDOUT__);
+		Reporter.log( "Resource path -> " + envPath, PRINT2STDOUT__);
+		Reporter.log( "Resource file -> " + envFile, PRINT2STDOUT__);		
+		
+		/** Create environment configuration */
+		env = new NetworkEnvironment( envPath, envFile, IOLoadingType.valueOf(loadingType) );
+		
+		Reporter.log( "Startup Selenium driver with \""+browser+"\".", PRINT2STDOUT__);
+		
+		/** Create Selenium WebDriver instance */
+		gui = env.getServer( gui_server );
+		seleniumWebDriver = new SeleniumWebDriver( gui.getBrowser( browser ), gui.getLink() );
+		
+		auth = new Authorization(seleniumWebDriver, TIMEOUT, ATTEMPT_TIMEOUT);
+	}	
+
+	@Test
+	@BeforeTest
+	@Parameters({"user"})
+	public void executeLogin(@Optional("superman") String user) throws FormException {
+	
+		Reporter.log( "Perform login procedure.", PRINT2STDOUT__);
+	
+		auth.login(gui.getUser( user ));
+	}
+	
+	@Test( dependsOnMethods = {"executeLogin"} )
+	@AfterTest
+	public void executeLogout() throws FormException {
+	
+		Reporter.log( "Perform logout procedure.", PRINT2STDOUT__);
+	
+		auth.logout();
+	}
+	
+	@AfterSuite
+	public void tearDown() {
+		
+		Reporter.log( "Close Selenium driver.", PRINT2STDOUT__);
+		
+		if (seleniumWebDriver != null)
+			seleniumWebDriver.close();
+	}	
+}
+
