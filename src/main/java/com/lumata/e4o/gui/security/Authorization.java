8b1bde40edff7b6148d1cdbc95709355de83c1bc
Vincenzo Marrazzo <vincenzo.marrazzo@lumatagroup.com>
2014-05-30 18:24:49 +0200

    double login handling

--- src/main/java/com/lumata/e4o/gui/security/Authorization.java
+++ src/main/java/com/lumata/e4o/gui/security/Authorization.java
@@ -15,6 +15,7 @@ import com.lumata.e4o.gui.common.Form;
  * <li> login
  * <li> license
  * <li> logout
+ * <li> double login
  */
 public class Authorization extends Form {
 
@@ -41,33 +42,85 @@ public class Authorization extends Form {
 		clickId( "gwt-debug-InputLoginUsername" ).
 		sendKeysById( "gwt-debug-InputLoginUsername", user ).
 		sendKeysById( "gwt-debug-InputLoginPassword", password ).
-		clickId( "gwt-debug-ButtonLoginAuthentication" ).
-		clickId( "gwt-debug-FormHomeInfo" );
+		clickId( "gwt-debug-ButtonLoginAuthentication" );
 		
-		closeLicenseDialog();
-				
+		// force a new login
+		doubleSession(true);
+		
+		Boolean clickedDialog = closeLicenseDialog();
+		
+		// if a click occurred for license warning :
+		// element "gwt-debug-FormHomeInfo" is already disappeared
+		if ( !clickedDialog)
+			clickId( "gwt-debug-FormHomeInfo" );
+		
 		return this;
+
 	}
 	
-	public Authorization closeLicenseDialog() {
+	/**
+	 * This methods looks for a popup related double session and handle it according <b>forceLogin</b> :<br>
+	 * <li> if TRUE, it forces new login ( another session can be dropped by DUT ) 
+	 * <li> if FALSE, it does not force a new login and throws an exception
+	 * 
+	 * @throws FormException 
+	 */
+	private void doubleSession(Boolean forceLogin) throws FormException {
+		
+		Alert confirmForceLogin = null;
+		 
+		try {
+			
+			confirmForceLogin = selenium.selectAlert();
+		    	
+			if ( confirmForceLogin != null )
+				if ( forceLogin ) 
+					confirmForceLogin.accept(); 
+				else
+					throw new FormException(getClass().getSimpleName() + " finds a \"double session\" and CANNOT force login!");
+			
+		} catch (NoAlertPresentException e) {
+			// nothing to do
+		}
+	}
+	
+	
+	/**
+	 * This method executes :<br>
+	 * <li> it searches for license warning box and close it
+	 * <li> if a click event occurs returns this information
+	 * 
+	 * @return Boolean information if a click event occurs
+	 * 
+	 * @throws FormException
+	 */
+	private Boolean closeLicenseDialog() throws FormException {
+		
+		Boolean closedLicenseDialog = null;
 		
 		try {
 			
 			long timeout = getTimeout();
 			
-			setTimeout( 1000 ).
-			
+			setTimeout( 1000 ).			
 			clickXPath( "//div[@class='gwt-DialogBox errorDialog']//button" ).
-		
 			setTimeout( timeout );
-		
-		} catch( NoSuchElementException | FormException e ) {
-		
-			status = true;
-		}
 			
-		return this;
-	
+			closedLicenseDialog = Boolean.TRUE;
+			
+		} catch( NoSuchElementException | FormException e ) {
+			
+			if ( e instanceof FormException )
+				if ( e.getMessage().contains("errorDialog") ) {
+					// do nothing
+					
+					closedLicenseDialog = Boolean.FALSE;
+				}
+				else // something happened
+					throw e;
+		}
+		
+		return closedLicenseDialog;
 	}
 	
 	public boolean refresh() throws FormException {
