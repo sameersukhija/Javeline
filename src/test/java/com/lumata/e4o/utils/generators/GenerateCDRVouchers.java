c711278ddf4f8012705c39b6015dd48f762ca072
Arcangelo Di Pasquale <arcangelo.dipasquale@lumatagroup.com>
2014-07-08 13:44:28 +0200

    Fix GenerateCDRVoucher dependence issue

--- src/test/java/com/lumata/e4o/utils/generators/GenerateCDRVouchers.java
+++ src/test/java/com/lumata/e4o/utils/generators/GenerateCDRVouchers.java
@@ -5,7 +5,6 @@ import org.testng.annotations.Test;
 import com.lumata.common.testing.exceptions.IOFileException;
 import com.lumata.common.testing.io.IOFileUtils.IOLoadingType;
 import com.lumata.e4o.utils.generators.CDRVouchersGenerator;
-import com.lumata.e4o.utils.generators.VouchersGenerator;
 
 @Deprecated /** depends from CDR deprecated class - to be refactoring using the new CDR class ( Vincenzo )*/
 public class GenerateCDRVouchers {
