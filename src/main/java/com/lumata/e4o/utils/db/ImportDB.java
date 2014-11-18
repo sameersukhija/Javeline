579aeb30479a0cd631e203059c376bd42f1797fe
davide.rognoni <davide.rognoni@lumatagroup.com>
2014-11-18 12:12:53 +0100

    This class is the main generic utility used to import the database tables (from "tenant", "global" or other)

---
+++ src/main/java/com/lumata/e4o/utils/db/ImportDB.java
@@ -0,0 +1,9 @@
+package com.lumata.e4o.utils.db;
+
+/**
+ * This class is the main generic utility used to import the database tables (from "tenant", "global" or other)
+ * 
+ */
+public class ImportDB {
+
+}
