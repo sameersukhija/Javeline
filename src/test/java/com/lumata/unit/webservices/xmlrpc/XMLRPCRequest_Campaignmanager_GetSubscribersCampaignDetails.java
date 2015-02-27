5a8237a08b77aaac83ab3646e0870980c63224f5
Arcangelo Di Pasquale <arcangelo.dipasquale@lumatagroup.com>
2015-02-27 14:15:23 +0100

    Added new XMLRPC calls

---
+++ src/test/java/com/lumata/unit/webservices/xmlrpc/XMLRPCRequest_Campaignmanager_GetSubscribersCampaignDetails.java
@@ -0,0 +1,62 @@
+package com.lumata.unit.webservices.xmlrpc;
+
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Optional;
+import org.testng.annotations.Parameters;
+import org.testng.annotations.Test;
+
+import com.lumata.common.testing.exceptions.NetworkEnvironmentException;
+import com.lumata.common.testing.io.IOFileUtils;
+import com.lumata.common.testing.system.NetworkEnvironment;
+import com.lumata.common.testing.system.Server;
+import com.lumata.common.testing.system.User;
+import com.lumata.e4o.webservices.xmlrpc.request.XMLRPCRequest;
+
+
+//import static org.hamcrest.Matchers.*;
+import static com.lumata.e4o.webservices.xmlrpc.request.XMLRPCComponent.*;
+import static com.lumata.e4o.webservices.xmlrpc.request.XMLRPCOption.*;
+import static com.lumata.e4o.webservices.xmlrpc.request.XMLRPCRequestMethods.*;
+//import static com.lumata.e4o.webservices.xmlrpc.response.XMLRPCResponseValidatorMethods.*;
+
+
+public class XMLRPCRequest_Campaignmanager_GetSubscribersCampaignDetails {
+	
+	NetworkEnvironment env;
+	Server actruleServer;
+	User superman;
+			
+	/* 	Initialize Environment */
+	@Parameters({"environment", "gui_server", "user"})
+	@BeforeClass
+	public void init( @Optional("E4O_VM") String environment, @Optional("actrule") String gui_server, @Optional("superman") String user ) throws NetworkEnvironmentException {		
+		
+		/** Create environment configuration */
+		env = new NetworkEnvironment( "input/environments", environment, IOFileUtils.IOLoadingType.RESOURCE );
+
+		actruleServer = env.getServer( gui_server );
+		
+		superman = actruleServer.getUser( user );
+		
+	}
+	
+	@Test(enabled=true, priority = 1 )
+	public void callXMLRPCCRequest() throws Exception {
+		
+		final String msisdn = "3399900001";
+		
+		XMLRPCRequest.campaignmanager_getSubscribersCampaignDetails().call( 	
+			actruleServer, 
+			xmlrpcBody(
+				authentication( superman ),
+				string( msisdn )
+			),
+			xmlrpcOptions(
+				storeRequestAsResource( "xmlrpc/request/", "request.xml" ),
+				storeResponseAsResource( "xmlrpc/response/", "response.xml" )	
+			)
+		);
+		
+	}
+	
+}
