dbaa569db1a074be953ddf4020353fa04892e1a0
Arcangelo Di Pasquale <arcangelo.dipasquale@lumatagroup.com>
2015-04-23 17:24:58 +0200

    Added Sending Mail from jenkins test case

--- src/test/java/com/lumata/e4o/regressions/gui/TestJenkins.java
+++ src/test/java/com/lumata/e4o/regressions/gui/TestJenkins.java
@@ -1,5 +1,7 @@
 package com.lumata.e4o.regressions.gui;
 
+import javax.mail.MessagingException;
+
 import org.json.JSONException;
 import org.json.JSONObject;
 import org.openqa.selenium.By;
@@ -9,6 +11,8 @@ import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 
 import com.lumata.common.testing.exceptions.JSONSException;
+import com.lumata.common.testing.network.Mail;
+import com.lumata.common.testing.network.MailClient;
 import com.lumata.common.testing.selenium.SeleniumWebDriver;
 import com.lumata.e4o.exceptions.FormException;
 import com.lumata.e4o.testing.common.TCOwner;
@@ -17,13 +21,24 @@ import com.lumata.e4o.testing.common.TCOwners;
 @TCOwners(
 	@TCOwner( name="Arcangelo Di Pasquale", email="arcangelo.dipasquale@lumatagroup.com" )
 )
+@Mail(
+		protocol = "smtp",
+		fromRecipient = "qa.e4o.all@lumatagroup.com",
+		toRecipients = { "arcangelo.dipasquale@lumatagroup.com" },
+		host = "internal.mailservices.lumata.int",
+		port = 25,
+		starttlsEnabled = false,
+		authorizationRequired = false,
+		user = "",
+		password = ""
+	)
 public class TestJenkins {
 
 	SeleniumWebDriver seleniumWebDriver;
 	
 	@Parameters("seleniumWebDriverParams")
-	@Test( enabled=true, priority = 1 )
-	public void checkMandatoryFields1( @Optional("") String seleniumWebDriverParams ) throws FormException, JSONException, JSONSException {
+	@Test( enabled=false, priority = 1 )
+	public void checkSeleniumWebDriverFromJenkins( @Optional("") String seleniumWebDriverParams ) throws FormException, JSONException, JSONSException {
 		
 		JSONObject jo = new JSONObject( seleniumWebDriverParams );
 		
@@ -34,10 +49,29 @@ public class TestJenkins {
 		seleniumWebDriver.getWrappedDriver().findElement( By.id( "lst-ib" ) ).sendKeys( "jenkins test" );
 		
 	}
-	
+
+	@Test( enabled=true, priority = 2 )
+	public void checkMailSendingFromJenkins() throws FormException, JSONException, JSONSException {
+		
+		try {
+			
+			Mail mail = this.getClass().getAnnotation( Mail.class );
+			
+			MailClient.getInstance( mail ).send( "subject" , "test mail sending from jenkins" );
+			
+		} catch( MessagingException e ) {
+			
+			System.out.println( e.getMessage() );
+			
+		}
+
+	}
+		
 	@AfterSuite
 	public void end() {
-		seleniumWebDriver.close();		
+				
+		if( null!= seleniumWebDriver ) { seleniumWebDriver.close(); }		
+	
 	}
 		
 }
