771e3a397f50480501e0ce72549f37d459b3dbf3
Arcangelo Di Pasquale <arcangelo.dipasquale@lumatagroup.com>
2015-04-30 10:49:01 +0200

    avoid spam

--- src/main/java/com/lumata/e4o/testing/common/TestNGReportListener.java
+++ src/main/java/com/lumata/e4o/testing/common/TestNGReportListener.java
@@ -53,8 +53,8 @@ import freemarker.template.TemplateException;
 @Mail(
 	protocol = "smtp",
 	fromRecipient = "qa.e4o.all@lumatagroup.com",
-	toRecipients = { "qa.e4o.all@lumatagroup.com" },
-//	toRecipients = { "arcangelo.dipasquale@lumatagroup.com" },
+//	toRecipients = { "qa.e4o.all@lumatagroup.com" },
+	toRecipients = { "arcangelo.dipasquale@lumatagroup.com" },
 	host = "internal.mailservices.lumata.int",
 	port = 25,
 	starttlsEnabled = false,
