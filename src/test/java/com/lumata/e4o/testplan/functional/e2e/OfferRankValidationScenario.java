c9df8d6b1ba9a5022da3d271f3725445f92698c5
parvinder.bhogra <parvinder.bhogra@lumatagroup.com>
2015-05-15 10:34:31 +0530

    made campaign model name small

--- src/test/java/com/lumata/e4o/testplan/functional/e2e/OfferRankValidationScenario.java
+++ src/test/java/com/lumata/e4o/testplan/functional/e2e/OfferRankValidationScenario.java
@@ -421,7 +421,7 @@ public class OfferRankValidationScenario extends ParentTestCase {
 		RulesForm rulesForm = new RulesForm(seleniumWebDriver, getRuleJson(), TIMEOUT,
 				ATTEMPT_TIMEOUT);
 		//setRuleName(Format.addTimestamp("Rule_"));
-		setRuleName("RuleEndToEnd1234");
+		setRuleName("RuleEndToEnd12345");
 		JSONArray ruleTypes = getRuleJson().getList();
 		for (int ruleTypeIndex = 0; ruleTypeIndex < ruleTypes.length(); ruleTypeIndex++) {
 			getRuleJson().setRuleById(ruleTypeIndex);
@@ -446,7 +446,7 @@ public class OfferRankValidationScenario extends ParentTestCase {
 							.value());
 			Assert.assertTrue(rulesForm.formIsValid());
 			rulesForm.saveRule();
-			status=rulesForm.isRuleNameInList("RuleEndToEnd1234");
+			status=rulesForm.isRuleNameInList("RuleEndToEnd12345");
 			rulesForm.close();
 		}
 
