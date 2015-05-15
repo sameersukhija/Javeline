85c3da60aa15168b747ca28a95195375fe5f1419
parvinder.bhogra <parvinder.bhogra@lumatagroup.com>
2015-05-15 11:28:49 +0530

    added thread.sleep after frame shifting

--- src/test/java/com/lumata/e4o/testplan/functional/e2e/OfferRankValidationScenario.java
+++ src/test/java/com/lumata/e4o/testplan/functional/e2e/OfferRankValidationScenario.java
@@ -225,6 +225,7 @@ public class OfferRankValidationScenario extends ParentTestCase {
 		Reporter.log("Open Token Tab.", LOG_TO_STD_OUT);
 		ccTokenForm.openTokenTab();
 		Reporter.log("Load offers allocated for the token", LOG_TO_STD_OUT);
+		try { Thread.sleep( 3000 );  } catch( Exception e) {}
 		ccTokenForm.loadOffersForCampaignName(getCampaignName());
 		if(ccTokenForm.verifyRanksOfOffers())
 		{
@@ -421,7 +422,7 @@ public class OfferRankValidationScenario extends ParentTestCase {
 		RulesForm rulesForm = new RulesForm(seleniumWebDriver, getRuleJson(), TIMEOUT,
 				ATTEMPT_TIMEOUT);
 		//setRuleName(Format.addTimestamp("Rule_"));
-		setRuleName("RuleEndToEnd78");
+		setRuleName("RuleEndToEnd789");
 		JSONArray ruleTypes = getRuleJson().getList();
 		for (int ruleTypeIndex = 0; ruleTypeIndex < ruleTypes.length(); ruleTypeIndex++) {
 			getRuleJson().setRuleById(ruleTypeIndex);
@@ -446,7 +447,7 @@ public class OfferRankValidationScenario extends ParentTestCase {
 							.value());
 			Assert.assertTrue(rulesForm.formIsValid());
 			rulesForm.saveRule();
-			status=rulesForm.isRuleNameInList("RuleEndToEnd78");
+			status=rulesForm.isRuleNameInList("RuleEndToEnd789");
 			rulesForm.close();
 		}
 
