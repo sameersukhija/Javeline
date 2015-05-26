d458aafae29d5bc2980aa4b01f5325f7b0824132
parvinder.bhogra <parvinder.bhogra@lumatagroup.com>
2015-05-26 12:18:33 +0530

    changed the testcase name as per the standard being followed

--- src/test/java/com/lumata/e4o/regressions/gui/TestSupplierForm.java
+++ src/test/java/com/lumata/e4o/regressions/gui/TestSupplierForm.java
@@ -30,7 +30,7 @@ public class TestSupplierForm extends ParentTestCase {
 	
 	@Parameters({"sup_jsonFilePath","sup_jsonFileName"})
 	@Test( enabled=TEST_ENABLED, timeOut=TESTNG_TIMEOUT, priority = 1 )
-	public void testSupplierCreation( @Optional("input/catalogmanager/suppliers") String sup_jsonFilePath, @Optional("supplierList") String sup_jsonFileName) throws FormException, JSONException, JSONSException {
+	public void testUc25_01CreateNewSupplier( @Optional("input/catalogmanager/suppliers") String sup_jsonFilePath, @Optional("supplierList") String sup_jsonFileName) throws FormException, JSONException, JSONSException {
 		Boolean status=false;
 		seleniumWebDriver.getWrappedDriver().manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
 		Reporter.log("Creation of \"Supplier Form\".", LOG_TO_STD_OUT);
@@ -74,7 +74,7 @@ public class TestSupplierForm extends ParentTestCase {
 		
 	}
 	@Test( enabled=TEST_ENABLED, timeOut=TESTNG_TIMEOUT, priority = 2 )
-	public void testEditSupplier() throws FormException, JSONException, JSONSException {
+	public void testUc25_02EditSupplier() throws FormException, JSONException, JSONSException {
 		seleniumWebDriver.getWrappedDriver().manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
 		Reporter.log("Edit of \"Supplier\".", LOG_TO_STD_OUT);
 		SuppliersForm suppliersForm = new SuppliersForm( seleniumWebDriver, TIMEOUT, ATTEMPT_TIMEOUT );
@@ -94,7 +94,7 @@ public class TestSupplierForm extends ParentTestCase {
 		}
 	}
 	@Test( enabled=TEST_ENABLED, timeOut=TESTNG_TIMEOUT, priority = 3 )
-	public void testDeleteSupplier() throws FormException, JSONException, JSONSException {
+	public void testUc25_03DeleteSupplier() throws FormException, JSONException, JSONSException {
 		seleniumWebDriver.getWrappedDriver().manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
 		Reporter.log("Delete Supplier", LOG_TO_STD_OUT);
 		SuppliersForm suppliersForm = new SuppliersForm( seleniumWebDriver, TIMEOUT, ATTEMPT_TIMEOUT );
